36f398e2b36a2ec670571d3c0b8afa46d34d07ae
plutoyi <yi.pluto@163.com>
2021-01-28 11:02:07 +0800

    fix

---
+++ laravel-ApiResponse/ApiResponse.php
@@ -0,0 +1,322 @@
+<?php
+/**
+ * Api响应格式输出
+ * 
+ * Created by PhpStorm.
+ * User: yisong.yang
+ * Date: 2018/7/25
+ * Time: 20:54
+ */
+namespace App\Services\ApiServices\Traits;
+
+use Symfony\Component\HttpFoundation\Response as FoundationResponse;
+use Response;
+
+trait ApiResponse
+{
+    /**
+     * @var string
+     */
+    protected $statusKeyword = 'status';
+    /**
+     * @var string
+     */
+    protected $dataKeyword = 'data';
+    /**
+     * @var string
+     */
+    protected $statusCodeKeyword = 'code';
+    /**
+     * @var string
+     */
+    protected $messageKeyword = 'message';
+    /**
+     * @var int
+     */
+    protected $statusCode = FoundationResponse::HTTP_OK;
+    /**
+     * @var string
+     */
+    protected $statusText;
+
+    /**
+     * array statusTexts
+     *
+     * @var array
+     */
+    public static $statusTexts = array(
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',            // RFC2518
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',          // RFC4918
+        208 => 'Already Reported',      // RFC5842
+        226 => 'IM Used',               // RFC3229
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',    // RFC7238
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Payload Too Large',
+        414 => 'URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',                                               // RFC2324
+        421 => 'Misdirected Request',                                         // RFC7540
+        422 => 'Unprocessable Entity',                                        // RFC4918
+        423 => 'Locked',                                                      // RFC4918
+        424 => 'Failed Dependency',                                           // RFC4918
+        425 => 'Reserved for WebDAV advanced collections expired proposal',   // RFC2817
+        426 => 'Upgrade Required',                                            // RFC2817
+        428 => 'Precondition Required',                                       // RFC6585
+        429 => 'Too Many Requests',                                           // RFC6585
+        431 => 'Request Header Fields Too Large',                             // RFC6585
+        451 => 'Unavailable For Legal Reasons',                               // RFC7725
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates (Experimental)',                      // RFC2295
+        507 => 'Insufficient Storage',                                        // RFC4918
+        508 => 'Loop Detected',                                               // RFC5842
+        510 => 'Not Extended',                                                // RFC2774
+        511 => 'Network Authentication Required',                             // RFC6585
+    );
+    
+    /**
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * reponse invalid
+     *
+     * @param void
+     * @return bool
+     */
+    public function isInvalid()
+    {
+        //暂时设置为false
+        return false;
+        return $this->statusCode < 100 || $this->statusCode >= 600;
+    }
+
+    /**
+     * @param $statusCode
+     * @return $this
+     */
+    public function setStatusCode($code, $text = null)
+    {
+        $this->statusCode = $code = (int) $code;
+        if ($this->isInvalid()) {
+            throw new \InvalidArgumentException(sprintf('The HTTP status code "%s" is not valid.', $code));
+        }
+        if (null === $text) {
+            $this->statusText = isset(self::$statusTexts[$code]) ? self::$statusTexts[$code] : 'unknown status';
+            return $this;
+        }
+        if (false === $text) {
+            $this->statusText = '';
+            return $this;
+        }
+        $this->statusText = $text;
+        return $this;
+    }
+
+    /**
+     * @param $data
+     * @param array $header
+     * @return mixed
+     */
+    public function respond($data, $header = [])
+    {
+        if ($this->debugEnabled()) {
+            $data  = array_merge($data, $this->getDebug());
+        }
+        return Response::json($data, $this->getStatusCode(), $header);
+    }
+
+    /**
+     * @param void
+     * @return mixed
+     */
+    protected function debugEnabled()
+    {
+        return false;
+        return app()->has('debugbar') && app('debugbar')->isEnabled();
+    }
+
+    /**
+     * @param void
+     * @return mixed
+     */
+    protected function getDebug()
+    {
+        return ['_debugbar' => app('debugbar')->getData()];
+    }
+
+    /**
+     * @param string $status
+     * @param array $data
+     * @param null $code
+     * @return mixed
+     */
+    public function status($status, array $data, $code = null)
+    {
+        if($code){
+            $this->setStatusCode($code);
+        } else {
+            $status == 'success' ? $this->setStatusCode(FoundationResponse::HTTP_OK) : $this->setStatusCode(FoundationResponse::HTTP_BAD_REQUEST);
+        }
+        $status = [$this->statusKeyword => $status, $this->statusCodeKeyword => $this->statusCode];
+        $data = array_merge($status, $data);
+        return $this->respond($data);
+    }
+
+    /**
+     * @param $message
+     * @param int $code
+     * @param string $status
+     * @return mixed
+     */
+    public function failed($message, $code = FoundationResponse::HTTP_BAD_REQUEST, $status = 'error')
+    {
+        return $this->setStatusCode($code)->message($message, $status);
+    }
+
+    /**
+     * @param $message
+     * @param string $status
+     * @return mixed
+     */
+    public function message($message, $status = "success", $data = [])
+    {
+        return $this->status($status, array_merge(compact('message'), compact('data')));
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    public function internalError($message = "Internal Error!")
+    {
+        return $this->failed($message, FoundationResponse::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    public function created($message = "created")
+    {
+        return $this->setStatusCode(FoundationResponse::HTTP_CREATED)->message($message);
+    }
+
+    /**
+     * @param $data
+     * @param string $status
+     * @return mixed
+     */
+    public function success($data, $status = "success")
+    {
+        return $this->status($status, compact($this->dataKeyword));
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    public function notFond($message = 'Not Fond!')
+    {
+        return $this->failed($message, Foundationresponse::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * Is response successful?
+     *
+     * @return bool
+     */
+    public function isSuccessful()
+    {
+        return $this->statusCode >= 200 && $this->statusCode < 300;
+    }
+
+    /**
+     * Is there a client error?
+     *
+     * @return bool
+     */
+    public function isClientError()
+    {
+        return $this->statusCode >= 400 && $this->statusCode < 500;
+    }
+
+    /**
+     * Was there a server side error?
+     *
+     * @return bool
+     */
+    public function isServerError()
+    {
+        return $this->statusCode >= 500 && $this->statusCode < 600;
+    }
+
+    /**
+     * Is the response OK?
+     *
+     * @return bool
+     */
+    public function isOk()
+    {
+        return 200 === $this->statusCode;
+    }
+
+    /**
+     * Is the response forbidden?
+     *
+     * @return bool
+     */
+    public function isForbidden()
+    {
+        return 403 === $this->statusCode;
+    }
+
+    /**
+     * Is the response a not found error?
+     *
+     * @return bool
+     */
+    public function isNotFound()
+    {
+        return 404 === $this->statusCode;
+    }
+
+}
